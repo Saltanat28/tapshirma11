0ae8b92022f47dff7934726a32d860cde0b62352
saltanat <saltanatnematillakyzy6@gmail.com>
2022-11-02 16:32:39 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Triangle triangle = new Triangle();
+        System.out.print("a = ");
+        triangle.a = new Scanner(System.in).nextInt();
+        System.out.print("b = ");
+        triangle.b = new Scanner(System.in).nextInt();
+        System.out.print("c = ");
+        triangle.c = new Scanner(System.in).nextInt();
+        triangle.area();
+
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle {
+    double a;
+    double b ;
+    double c;
+
+    void  area(){
+        double s = (a+b+c);
+        double s1 =s/2;
+        double s2 = Math.sqrt(s1*(s1-a)*(s1-b)*(s1-c));
+        System.out.println("Squear of the Triangle: "+s2);
+    }
 }
